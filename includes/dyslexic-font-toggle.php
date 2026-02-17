1e786d4c4aac840c6a8b6b01c2192622988f96f2
Gabriel Sykes <gabri@fruityslab.tailf71ff1.ts.net>
2026-02-17 14:38:18 +0000

    Add more repeating elements to their own php includes.

---
+++ includes/dyslexic-font-toggle.php
@@ -0,0 +1,7 @@
+<button
+    id="dyslexicFontToggle"
+    class="dyslexic-font-toggle"
+    aria-label="Toggle Dyslexia Friendly Font"
+>
+    <p class="toggle-text">Toggle Dyslexia<br />Friendly Font</p>
+</button>
